1ce1e3f6bbf240dc6f9e29770cbebf66e3ac0236
Michał Sałaban <michal@salaban.info>
2020-02-27 03:22:48 +0100

    Add logo

--- source/conf.py
+++ source/conf.py
@@ -96,6 +96,8 @@ html_theme = 'alabaster'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+html_logo = '_static/vigla.svg'
+
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
 #
